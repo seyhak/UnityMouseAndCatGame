2064521d48e630659af48daf54086ef71d4d75a3
seyhak <seyhak.ly@gmail.com>
2019-08-21 13:02:10 +0200

    Add files via upload

---
+++ Models/TargetTxt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetTxt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fading());
+	}
+	IEnumerator Fading()
+    {
+        yield return new WaitForSeconds(6);
+        gameObject.SetActive(false);
+    }
+	
+}
---
+++ Models/YouAreDead.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YouAreDead : MonoBehaviour {
+    Text text;
+    float textSpeed = 0.01f;
+    // Use this for initialization
+    void Start () {
+       text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.color = Color.Lerp(text.color, Color.black, textSpeed);
+	}
+}
